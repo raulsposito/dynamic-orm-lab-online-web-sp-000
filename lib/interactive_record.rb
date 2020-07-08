c8c77830c38d1bec63c197bd6915653f180520d8
Raul Sposito <raulsposito@gmail.com>
2020-07-08 20:17:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,4 +28,13 @@ class InteractiveRecord
     DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+  def table_name_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+  
+
 end
